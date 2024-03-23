285dfab4924f2b7d85f05e1817082cc9774da0e2
dgz <dagouzhi@qq.com>
2024-03-23 17:26:50 +0800

    更新

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import '@tarojs/rn-supporter/entry-file.js'
-import SplashScreen from 'react-native-splash-screen';
-SplashScreen.hide();
+import App from '@tarojs/rn-supporter/entry-file.js'
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
+import { MiniAppsEnginesProvider } from '@htyf-mp/engines'
+
+const Root = () => {
+  return <MiniAppsEnginesProvider>
+    <App />
+  </MiniAppsEnginesProvider>
+}
+
+AppRegistry.registerComponent(appName, () => Root)
