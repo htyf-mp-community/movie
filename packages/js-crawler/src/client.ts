94072478af9db3e8888f30d4eaf90194f20e1248
dgz <dagouzhi@qq.com>
2024-04-20 11:30:17 +0800

    fix

--- packages/js-crawler/src/client.ts
+++ packages/js-crawler/src/client.ts
@@ -30,7 +30,7 @@ class SDK extends EventEmitter {
       return this.getDetail();
     } else if (/^\/v_play\/.*\.html$/gi.test(pathname)) {
       return this.getPlayUrl();
-    } else if (/^\/xsseanmch/gi.test(pathname)) {
+    } else if (/^\/xsssbeanmch/gi.test(pathname)) {
       return this.getSearch();
     } else if (/^\/(movie_bt|dbtop250|zuixindianying|dongmanjuchangban|benyueremen|gcj|meijutt|hanjutv|fanju)/gi.test(pathname)) {
       return this.getType();
