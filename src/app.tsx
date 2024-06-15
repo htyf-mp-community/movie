2ff7f903da3f86704b92fecd555b5622e6dfc1bd
dgz <dagouzhi@qq.com>
2024-06-16 00:31:42 +0800

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,35 @@
 import { useEffect } from 'react';
 import { useDidHide, useDidShow, usePageNotFound } from '@tarojs/taro';
-import {StoreProvider, useAppSelector, navigate} from '@/_UIHOOKS_';
-import './app.scss'
+import { StoreProvider, useAppSelector, navigate } from '@/_UIHOOKS_';
+import './app.scss';
 
 function RootFix(props: any) {
-	const appStore = useAppSelector(i => i);
-	useEffect(() => {
-		console.log(appStore)
-	}, [])
-	return <>{props.children}</>
+  const appStore = useAppSelector(i => i);
+  useEffect(() => {
+    console.log(appStore);
+  }, []);
+  return <>{props.children}</>;
 }
 
 function App(props) {
-	// 可以使用所有的 React Hooks
-  useEffect(() => {})
+  // 可以使用所有的 React Hooks
+  useEffect(() => {});
 
   // 对应 onShow
-  useDidShow(() => {})
+  useDidShow(() => {});
 
   // 对应 onHide
-  useDidHide(() => {})
-	usePageNotFound(() => {
-		navigate.redirectTo({
-			url: navigate.routes.pages['404'],
-		})
-	})
-	return (
-		<StoreProvider>
-				<RootFix>{props.children}</RootFix>
-		</StoreProvider>
-	)
+  useDidHide(() => {});
+  usePageNotFound(() => {
+    navigate.redirectTo({
+      url: navigate.routes.pages['404'],
+    });
+  });
+  return (
+    <StoreProvider>
+      <RootFix>{props.children}</RootFix>
+    </StoreProvider>
+  );
 }
 
-export default App
+export default App;
